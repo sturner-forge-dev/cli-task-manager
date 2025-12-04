cc1774d7249a459fe5233e058cc630f53f6d6c7f
Steven Turner <sturner@forge-applications.io>
2025-12-03 17:58:17 -0700

    add utility functions for loading and saving tasks

---
+++ internal/tasks/storage.go
@@ -0,0 +1,61 @@
+package tasks
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const defaultFile = "tasks.txt"
+
+func LoadTasks(path string) ([]Task, error) {
+	if path == "" {
+		path = defaultFile
+	}
+
+	f, err := os.Open(path)
+	if errors.Is(err, os.ErrNotExist) {
+		return []Task{}, nil
+	} else if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	tasks := make([]Task, 0)
+	scanner := bufio.NewScanner(f)
+	id := 1
+	for scanner.Scan() {
+		desc := strings.TrimSpace(scanner.Text())
+		if desc == "" {
+			continue
+		}
+		task := NewTask(id, desc)
+		tasks = append(tasks, task)
+		id++
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func SaveTasks(path string, entries []Task) error {
+	if path == "" {
+		path = defaultFile
+	}
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	writer := bufio.NewWriter(f)
+	for _, task := range entries {
+		if _, err := fmt.Fprintln(writer, strings.TrimSpace(task.Description)); err != nil {
+			return err
+		}
+	}
+	return writer.Flush()
+}
